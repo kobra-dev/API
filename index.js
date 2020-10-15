806af39d320cb105dcbf7c18873802005a5f27a7
Pranav Teegavarapu <pranavnt@outlook.com>
2020-10-15 10:20:37 -0700

    structure of dataset route

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const app = require("express")();
 const multer = require("multer");
+const AWS = require("aws-sdk");
+const port = 3000;
 
 var upload = multer();
+
+app.post("/datasetUpload", upload.array(), function (req, res, next) {
+  //get file and upload to digitalocean spaces
+  const dataset = req.files[0];
+  const user = req.body.username;
+  const datasetName = req.body.datasetname;
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
